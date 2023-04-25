c5d678bc10cb8a700cad4f2fb8108cd067326d9b
Dylan Elens <dylanelens@hotmail.com>
2023-04-25 16:18:13 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use actix_cors::Cors;
 use actix_multipart::Multipart;
-use actix_web::{
-    http::header::CONTENT_LENGTH, http::header::CONTENT_TYPE, web, App, HttpRequest, HttpResponse,
-    HttpServer,
-};
+use actix_web::{HttpServer, HttpRequest, HttpResponse, App, web};
 use futures_util::TryStreamExt as _;
-use image::{imageops::FilterType, DynamicImage};
-use mime::{Mime, IMAGE_GIF, IMAGE_JPEG, IMAGE_PNG};
 use std::io::Result;
 use std::path::Path;
 use tokio::fs;
@@ -20,10 +15,10 @@ async fn main() -> Result<()> {
     }
 
     HttpServer::new(|| {
-        let cors = Cors::permissive();
+        let cors = Cors::permissive().allow_any_origin();
         App::new()
             .wrap(cors)
-            .route("/", web::get().to(root))
+            .route("/", web::get().to(healthcheck))
             .route("/upload", web::post().to(upload))
     })
     .bind(("127.0.0.1", 8080))?
@@ -31,60 +26,14 @@ async fn main() -> Result<()> {
     .await
 }
 
-async fn root() -> String {
+async fn healthcheck() -> String {
     "Server is up and running.".to_string()
 }
 
-async fn upload(mut payload: Multipart, req: HttpRequest) -> HttpResponse {
-    println!("we have arrived");
-    // 1. limit file size             done
-    // 2. limit file count            done
-    // 3. limit file type             done
-    // 4. check if correct field      done
-    // 5. convert to *gif             done
-    // 6. save under random name      done
-    let content_length: usize = match req.headers().get(CONTENT_LENGTH) {
-        Some(header_value) => header_value.to_str().unwrap_or("0").parse().unwrap(),
-        None => "0".parse().unwrap(),
-    };
-
-    let max_file_count: usize = 3;
-    let max_file_size: usize = 10_000;
-    let legal_filetypes: [Mime; 3] = [IMAGE_PNG, IMAGE_JPEG, IMAGE_GIF];
-    let mut current_count: usize = 0;
+async fn upload(mut payload: Multipart, _req: HttpRequest) -> HttpResponse {
     let dir: &str = "./upload/";
-
-    // if content_length > max_file_size {
-    //     return HttpResponse::BadRequest().into();
-    // }
-
     loop {
-        if current_count == max_file_count {
-            break;
-        }
         if let Ok(Some(mut field)) = payload.try_next().await {
-            let filetype: Option<&Mime> = field.content_type();
-            if filetype.is_none() {
-                continue;
-            }
-            if !legal_filetypes.contains(&filetype.unwrap()) {
-                continue;
-            }
-            if field.name() != "avatar" {
-                continue;
-            }
-
-            // println!("content_length: {:#?}", content_length);
-            // println!("{}. picture:", current_count);
-            // println!("name {}", field.name()); // &str
-            // println!("headers {}", field.headers());
-            // println!("content type {}", field.content_type()); // &Mime
-            // println!("content type is mime::IMAGE_PNG {}", field.content_type() == &IMAGE_PNG);
-
-            // In a multipart/form-data body, the HTTP Content-Disposition general header is a header that can be used on the subpart of a multipart body to give information about the field it applies to. The subpart is delimited by the boundary defined in the Content-Type header. Used on the body itself, Content-Disposition has no effect.
-            // println!("content disposition {}", field.content_disposition()); // &ContentDisposition
-
-            // println!("filename {}", field.content_disposition().get_filename().unwrap()); // Option<&str>
 
             let destination: String = format!(
                 "{}{}-{}",
@@ -97,24 +46,9 @@ async fn upload(mut payload: Multipart, req: HttpRequest) -> HttpResponse {
             while let Ok(Some(chunk)) = field.try_next().await {
                 let _ = saved_file.write_all(&chunk).await.unwrap();
             }
-
-            web::block(move || async move {
-                let uploaded_img: DynamicImage = image::open(&destination).unwrap();
-                let _ = fs::remove_file(&destination).await.unwrap();
-                uploaded_img
-                    .resize_exact(200, 200, FilterType::Gaussian)
-                    .save(format!("{}{}.gif", dir, Uuid::new_v4().to_string()))
-                    .unwrap();
-            })
-            .await
-            .unwrap()
-            .await;
         } else {
             break;
         }
-        current_count += 1;
-        println!("incremented");
     }
-
-    HttpResponse::Ok().into()
+    HttpResponse::NoContent().into()
 }
