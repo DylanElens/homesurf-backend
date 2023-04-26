31b97971339fe90fcb3a517ce549d831674ae87e
Dylan Elens <dylanelens@hotmail.com>
2023-04-26 17:55:20 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_cors::Cors;
+use actix_files::NamedFile;
 use actix_multipart::Multipart;
 use actix_multipart_demo::{create_file, establish_connection, list_files};
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
@@ -8,7 +9,6 @@ use std::path::{Path, PathBuf};
 use tokio::fs;
 use tokio::io::AsyncWriteExt as _;
 use uuid::Uuid;
-use actix_files::NamedFile;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -46,12 +46,17 @@ async fn upload(mut payload: Multipart, _req: HttpRequest) -> HttpResponse {
                 Uuid::new_v4(),
                 field.content_disposition().get_filename().unwrap()
             );
-            let result = create_file(
+
+            match create_file(
                 &mut conn,
                 field.content_disposition().get_filename().unwrap(),
                 &destination,
                 &field.content_type().unwrap().to_string(),
-            );
+            ) {
+                Ok(_) => print!(""),
+                Err(_) => println!(),
+            }
+
             let mut saved_file: fs::File = fs::File::create(&destination).await.unwrap();
             while let Ok(Some(chunk)) = field.try_next().await {
                 let _ = saved_file.write_all(&chunk).await.unwrap();
@@ -64,13 +69,14 @@ async fn upload(mut payload: Multipart, _req: HttpRequest) -> HttpResponse {
 }
 
 async fn get_file(req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = PathBuf::from(format!("{}{}","./upload/".to_owned().as_str() ,req.match_info().query("filename")));
+    let path: PathBuf = PathBuf::from(format!(
+        "{}{}",
+        "./upload/".to_owned().as_str(),
+        req.match_info().query("filename")
+    ));
     println!("here we are {:?}", path);
     Ok(NamedFile::open(path)?)
 }
-
-
-
 
 async fn download_file(req: HttpRequest) -> Result<NamedFile> {
     let path: PathBuf = PathBuf::from(format!(
@@ -86,4 +92,3 @@ async fn download_file(req: HttpRequest) -> Result<NamedFile> {
         },
     ))
 }
-
